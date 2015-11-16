fec987eb98e074bbb6fb143ad3cd8129dcf4d804
Robert Marchese <bobm123@gmail.com>
2015-11-16 07:07:37 -0500

    Adds the /cities route returning some json

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var app = express();
 
+
 app.get('/', function(request, response) {
   response.send('OK');
-  //throw 'Error'
 });
 
-//app.listen(3000);
+
+app.get('/cities', function(request, response) {
+  var cities = ['Lotopia', 'Caspiana', 'Indigo'];
+  response.json(cities);
+});
+
 
 module.exports = app
+
+// some snippets (find a plug in)
+/* expr snippet
+var express = require('express');
+var app = express();
+
+app.listen(3000, function() {
+  console.log('listen on 3000')
+});
+*/
+
+/* new route snippet
+app.get('/', function(request, response) {
+  //response.send('OK');
+});
+
+*/
+
--- test.js
+++ test.js
@@ -3,6 +3,11 @@
 //    npm install supertest --save-dev
 var request = require('supertest');
 
+
+// also see mocha -w to watch files and run these test
+// whenever:
+// ./node_modules/mocha/bin/mocha app.js test.js
+
 var app = require('./app');
 
 
@@ -13,13 +18,45 @@ describe('Requests to the root path', function() {
     //uses function chaining to setup a list of test steps
     request(app)
       .get('/')
-      .expect(200)
+      .expect(200, done);
+/* done); (2nd arg above) is short for this:
       .end(function(error) {
         if(error) throw error;
         done();
       });
-
+*/
   });
 
 });
 
+describe('Listing cities on /cities', function() {
+
+  it('Returns 200 status code', function(done) {
+    request(app)
+      .get('/cities')
+      .expect(200, done);
+  });
+
+  it('Returns JSON format', function(done) {
+    request(app)
+      .get('/cities')
+      .expect('Content-Type', /json/, done);
+  });
+
+  it('Returns initial cities', function(done) {
+    request(app)
+      .get('/cities')
+      .expect(JSON.stringify(['Lotopia', 'Caspiana', 'Indigo']), done);
+  });
+
+});
+
+
+/* test case template
+describe('', function() {
+
+  it('', function(done) {
+
+  });
+});
+*/
